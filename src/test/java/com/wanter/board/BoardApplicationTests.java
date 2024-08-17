625b33119a0ec5a2f2a613bce4db0b7188cfb044
obj <dhqudwo2976@naver.com>
2024-08-17 13:34:09 +0900

    초기설정

---
+++ src/test/java/com/wanter/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanter.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
